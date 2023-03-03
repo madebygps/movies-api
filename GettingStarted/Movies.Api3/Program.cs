5abc97c78f8728566fb563c3f7046985f7c8654d
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2023-03-03 08:22:40 -0500

    implemented basic api

--- GettingStarted/Movies.Api3/Program.cs
+++ GettingStarted/Movies.Api3/Program.cs
@@ -1,5 +1,4 @@
-﻿using Movies.Application.Repositories;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Movies.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
 
 
 var app = builder.Build();
--- GettingStarted/Movies.Contracts/Responses/MoviesResponse.cs
+++ GettingStarted/Movies.Contracts/Responses/MoviesResponse.cs
@@ -3,7 +3,7 @@ namespace Movies.Contracts.Responses
 {
 	public class MoviesResponse
 	{
-		public required IEnumerable<MoviesResponse> Items { get; init; } = Enumerable.Empty<MoviesResponse>();
+		public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
 	}
 }
 
